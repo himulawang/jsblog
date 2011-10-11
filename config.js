dfbf2234b18bf2e483546c5eb337a25d5043cfbc
ila <ila@ila-empire.(none)>
2011-10-11 22:39:34 +0800

    change config

--- config.js
+++ config.js
@@ -15,9 +15,9 @@ if (env === 'local') {
     G.URL_ADMIN = 'ws://jsblog:' + G.PORT;
 } else if (env === 'live') {
     G.PORT = '80';
-    G.URL_ADMIN = 'ws://gi.no.de:' + G.PORT;
+    G.URL_ADMIN = 'ws://ila.no.de:' + G.PORT;
 }
 
-G.DB = 'mongodb://localhost/jsblog';
+G.DB = 'mongodb://127.0.0.1/jsblog';
 G.PROTOCOL = 'jsblog';
 
